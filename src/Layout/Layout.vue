57eedf93fb3d4cfe97966bd73f46d1ca76faa663
mariotab <mario_tab@i.ua>
2020-02-24 10:41:42 +0200

    add bootstrap and simple layuot

---
+++ src/Layout/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-3">
+        <p> Test</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
